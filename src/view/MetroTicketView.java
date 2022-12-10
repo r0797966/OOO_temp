1623bd624f4406dfed8c48f911ef3fe5d7b00d43
Robyn <r0797966@ucll.be>
2022-12-10 15:20:02 +0100

    new card button

--- src/view/MetroTicketView.java
+++ src/view/MetroTicketView.java
@@ -3,13 +3,16 @@ package view;
 import controller.MetroTicketViewController;
 import javafx.scene.Group;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
 import model.facade.MetroFacade;
 
 import java.util.ArrayList;
 
-public class MetroTicketView {
+public class MetroTicketView extends GridPane {
 	private Stage stage = new Stage();		
 		
 	public MetroTicketView(MetroTicketViewController metroTicketViewController) {
@@ -18,17 +21,24 @@ public class MetroTicketView {
 		stage.setX(5);
 		stage.setY(5);
 		Group root = new Group();
-		Scene scene = new Scene(root, 650, 350);			
+		Scene scene = new Scene(root, 650, 350);
 		stage.setScene(scene);
 		stage.sizeToScene();			
 		stage.show();
 
 		metroTicketViewController.setView(this);
+
+		Button newCardButton = new Button("New metro card");
+		newCardButton.setOnAction(e -> {
+			System.out.println("New metro card");
+		});
+		root.getChildren().add(newCardButton);
+
 	}
 
 	// UPDATEMETROCARDIDLIST(IDs ArrayList<Integer>)
 	public void updateMetroCardIdList(ArrayList<Integer> ids) {
 		// TODO: refresh doen van de choicebox met metrocard ID’s -> hebben we nog niet
-		System.out.println("implement updateMetroCardIdList in MetroTicketView");
+		System.out.println("implement (nog niet mogelijk nog geen view");
 	}
 }
